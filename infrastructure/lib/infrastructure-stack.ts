37f5b2ac6f24ba831e56c22908b32ec4915eef02
akhudiak <next.teacher@lectrum.io>
2024-06-27 19:37:27 +0100

    finish

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -1,50 +1,53 @@
-import * as cdk from 'aws-cdk-lib';
-import * as s3 from 'aws-cdk-lib/aws-s3';
-import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
-import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
-import { BucketDeployment, Source } from 'aws-cdk-lib/aws-s3-deployment';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import * as s3 from "aws-cdk-lib/aws-s3";
+import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
+import * as origins from "aws-cdk-lib/aws-cloudfront-origins";
+import { BucketDeployment, Source } from "aws-cdk-lib/aws-s3-deployment";
+import { Construct } from "constructs";
 
 export class InfrastructureStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const websiteBucket = new s3.Bucket(this, 'WebsiteBucket', {
-      websiteIndexDocument: 'index.html',
-      publicReadAccess: false
+    const websiteBucket = new s3.Bucket(this, "WebsiteBucket", {
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      objectOwnership: s3.ObjectOwnership.OBJECT_WRITER
     });
 
-    const originAccessIdentity = new cloudfront.OriginAccessIdentity(this, 'OAI');
+    const originAccessIdentity = new cloudfront.OriginAccessIdentity(
+      this,
+      "OAI"
+    );
 
-    const distribution = new cloudfront.Distribution(this, 'Distribution', {
+    const distribution = new cloudfront.Distribution(this, "Distribution", {
       defaultBehavior: {
         origin: new origins.S3Origin(websiteBucket, {
           originAccessIdentity
         }),
         allowedMethods: cloudfront.AllowedMethods.ALLOW_GET_HEAD_OPTIONS,
-        viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS
+        viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
       },
-      defaultRootObject: 'index.html',
+      defaultRootObject: "index.html",
       errorResponses: [
         {
           httpStatus: 403,
           responseHttpStatus: 200,
-          responsePagePath: '/index.html'
+          responsePagePath: "/index.html",
         },
         {
           httpStatus: 404,
           responseHttpStatus: 200,
-          responsePagePath: '/index.html'
-        }
-      ]
+          responsePagePath: "/index.html",
+        },
+      ],
     });
 
-    websiteBucket.grantRead(originAccessIdentity)
+    websiteBucket.grantRead(originAccessIdentity);
 
-    new BucketDeployment(this, 'StaticWebsiteBucketDeployment', {
-      sources: [Source.asset('../web-app/public')],
+    new BucketDeployment(this, "StaticWebsiteBucketDeployment", {
+      sources: [Source.asset("../web-app/public")],
       destinationBucket: websiteBucket,
-      distribution
-    })
+      distribution,
+    });
   }
 }
