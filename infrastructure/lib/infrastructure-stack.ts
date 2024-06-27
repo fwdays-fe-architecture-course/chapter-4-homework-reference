8144d6a991cfcd9379360d8999a0a07b81a16db1
akhudiak <next.teacher@lectrum.io>
2024-06-27 19:05:04 +0100

    finished with the deployment infra

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -2,7 +2,7 @@ import * as cdk from 'aws-cdk-lib';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
 import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
-
+import { BucketDeployment, Source } from 'aws-cdk-lib/aws-s3-deployment';
 import { Construct } from 'constructs';
 
 export class InfrastructureStack extends cdk.Stack {
@@ -14,15 +14,37 @@ export class InfrastructureStack extends cdk.Stack {
       publicReadAccess: false
     });
 
+    const originAccessIdentity = new cloudfront.OriginAccessIdentity(this, 'OAI');
+
     const distribution = new cloudfront.Distribution(this, 'Distribution', {
       defaultBehavior: {
-        origin: new origins.S3Origin(websiteBucket),
+        origin: new origins.S3Origin(websiteBucket, {
+          originAccessIdentity
+        }),
         allowedMethods: cloudfront.AllowedMethods.ALLOW_GET_HEAD_OPTIONS,
         viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS
       },
-      defaultRootObject: 'index.html'
+      defaultRootObject: 'index.html',
+      errorResponses: [
+        {
+          httpStatus: 403,
+          responseHttpStatus: 200,
+          responsePagePath: '/index.html'
+        },
+        {
+          httpStatus: 404,
+          responseHttpStatus: 200,
+          responsePagePath: '/index.html'
+        }
+      ]
     });
 
-    
+    websiteBucket.grantRead(originAccessIdentity)
+
+    new BucketDeployment(this, 'StaticWebsiteBucketDeployment', {
+      sources: [Source.asset('../web-app/public')],
+      destinationBucket: websiteBucket,
+      distribution
+    })
   }
 }
